afec70978356cfc81aa20dc27326cbe6ba5fb4ba
YanqiuLi05 <Liy905@newschool.edu>
2025-03-27 03:37:01 -0400

    assignment 3 final

--- Assignment3.js
+++ Assignment3.js
@@ -1,56 +1,90 @@
 let video;
 let scaleFactor = 1; 
 let targetScale = 1; 
+let img;
+let showImage = false; 
+
+function preload() {
+  img = loadImage('tinydoggy.png');
+}
 
 function setup() {
   createCanvas(640, 480);
   video = createCapture(VIDEO);
   video.size(640, 480);
-  video.hide(); 
+  video.hide();
 }
 
 function draw() {
   background(0);
-
   image(video, 0, 0, width, height);
-  
-  let w = 50, h = 40; // original rectangle size
-  let x = mouseX
-  let y = mouseY//move with the mouse
-  
 
-  let smallClip = video.get(x, y, w, h);//pixel captured from the rectangle
+  // Gradually increase size
+  scaleFactor = lerp(scaleFactor, targetScale, 0.02);
 
+  let w = 50, h = 40; // Original rectangle size
+  let x = mouseX;
+  let y = mouseY; // Move with the mouse
+
+  let smallClip = video.get(x, y, w, h); // Capture pixels from the video
   
-  scaleFactor = lerp(scaleFactor, targetScale, 0.02);// Gradually increasing size
+  
+  let grayClip = applyGrayscale(smallClip);//greyscale in the clip 
   
   let enlargedW = w * scaleFactor;
   let enlargedH = h * scaleFactor;
-  
   let enlargedX = mouseX - enlargedW / 2;
-  let enlargedY = mouseY - enlargedH / 2;//small clip stays in center of the mouse
-  
-  
-  image(smallClip, enlargedX, enlargedY, enlargedW, enlargedH);
-  //enlarged picture
- 
+  let enlargedY = mouseY - enlargedH / 2; // Small clip stays in center of the mouse
 
- if (targetScale >= 3) {
+  let imgOriginalW = img.width;
+  let imgOriginalH = img.height;
+  
+  let imgScaledW = imgOriginalW * scaleFactor;
+  let imgScaledH = imgOriginalH * scaleFactor;
+  let imgX = 500 - imgScaledW / 2;
+  let imgY = 400 - imgScaledH / 2;//image size change with scalefactor
+
+  
+  let isInsideClip = (
+    imgX > enlargedX &&
+    imgX + imgScaledW < enlargedX + enlargedW &&
+    imgY > enlargedY &&
+    imgY + imgScaledH < enlargedY + enlargedH
+  );//if image is in clip area
+  image(grayClip, enlargedX, enlargedY, enlargedW, enlargedH);
+
+  if (isInsideClip) {
+    image(img, imgX, imgY, imgScaledW, imgScaledH);
+  }// Only draw the image if it is completely inside the smallClip
+
+  
+  if (targetScale >= 3) {
     growing = false;
   } else if (targetScale <= 1) {
     growing = true;
   }
 
- 
   if (growing) {
     targetScale += 0.005;
   } else {
     targetScale -= 0.005;
-  }//Once the size bigger than 3, it starts to shrink
+  }
+}//shink only it grows bigger then 3
 
 
-  
- 
+function applyGrayscale(inputImg) {
+  inputImg.loadPixels();
+  for (let i = 0; i < inputImg.pixels.length; i += 4) {
+    let r = inputImg.pixels[i];
+    let g = inputImg.pixels[i + 1];
+    let b = inputImg.pixels[i + 2];
+    
+    let brightness = (r + g + b) / 3; 
 
-  filter(GRAY);
-}
+    inputImg.pixels[i] = brightness;
+    inputImg.pixels[i + 1] = brightness;
+    inputImg.pixels[i + 2] = brightness;
+  }
+  inputImg.updatePixels();
+  return inputImg;
+}//greyscale filter
